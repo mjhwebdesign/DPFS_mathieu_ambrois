8a070c45cc0d01fd0050f03d692ee6ca19fa4939
mathieu.ambrois <ambrois.mathieu@gmail.com>
2026-02-11 19:48:40 -0300

    Add active class to current page

--- EIKON/controllers/indexController.js
+++ EIKON/controllers/indexController.js
@@ -1,15 +1,15 @@
 const indexController = {
  index: function (req, res, next) {
-  res.render("index");
+  res.render("index", { currentPath: req.path });
  },
  cart: function (req, res, next) {
-  res.render("products/productCart");
+  res.render("products/productCart", { currentPath: req.path });
  },
  login: function (req, res, next) {
   res.render("users/userlogin-userRegister");
  },
  logout: function (req, res, next) {
-  res.render("index");
+  res.render("index", { currentPath: req.path });
  },
 };
 module.exports = indexController;
