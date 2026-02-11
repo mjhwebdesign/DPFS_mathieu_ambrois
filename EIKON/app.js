cfc3fba25a2e937dca5c896bcf3c03cb0e4a704d
mathieu.ambrois <ambrois.mathieu@gmail.com>
2026-02-11 17:30:25 -0300

    Add Static path to public folder. Update paths to static files in views

--- EIKON/app.js
+++ EIKON/app.js
@@ -10,6 +10,9 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
